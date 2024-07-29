baf8d1d627b9a3943c0da130907289880d2fa0b7
chiragdcmarix <77272627+chiragdcmarix@users.noreply.github.com>
2024-07-29 15:04:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is a first file and  sample Python Web App running on Flask Framework!"
+    return "Hello, this is a first file and  sample Python Web App running on Flask Framework!" It works from jinesh patel Devops enginner...
 
