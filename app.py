b81df7a4438e163587521813953ffd4b68d5f8fe
chiragdcmarix <77272627+chiragdcmarix@users.noreply.github.com>
2024-07-29 15:41:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is a first file and  sample Python Web App running on Flask Framework!"
-    return " This is also form Devops team and IT team"
-
+    return "Hello, this is a first file and  sample Python Web App running on Flask Framework!" "It is most likely Pipeline for Devops and Azure."
+    
